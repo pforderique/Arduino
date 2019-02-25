9544660c77dcb3613a3937d8fbcdf14990f4014e
fabrizzioorderique <fabrizzioorderique@gmail.com>
2019-02-25 09:36:54 -0500

    testing commit

--- TestArduino/TestArduino.ino
+++ TestArduino/TestArduino.ino
@@ -1,11 +1,19 @@
-/*
+/.*
  * This is is a test project to make sure that it uploads correctly 
  * to GitHub and to see where it saves my code
  */
+const int led = 13;
 void setup() {
-
+  pinMode(led,OUTPUT);
 }
 
 void loop() {
+  blinkLed(led,500);
+}
 
+void blinkLed(int ledPin, int Delay){
+  digitalWrite(ledPin,HIGH);
+  delay(Delay);
+  digitalWrite(ledPin,LOW);
+  delay(Delay);
 }
