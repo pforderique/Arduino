4badde3d5d956760f45c98a57507a2d0d4433c79
fabrizzioorderique <45519489+fabrizzioorderique@users.noreply.github.com>
2019-02-25 08:57:27 -0500

    Got rid of the initial setup and loop comments

--- TestArduino/TestArduino.ino
+++ TestArduino/TestArduino.ino
@@ -3,11 +3,9 @@
  * to GitHub and to see where it saves my code
  */
 void setup() {
-  // put your setup code here, to run once:
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
