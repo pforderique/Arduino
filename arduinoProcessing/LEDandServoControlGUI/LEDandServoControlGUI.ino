b529e91e07a5f5274eff5d61e43a1112b24ad973
fabrizzioorderique <fabrizzioorderique@gmail.com>
2020-03-15 13:37:01 -0400

    added new servocontrolGUI

--- arduinoProcessing/LEDandServoControlGUI/LEDandServoControlGUI.ino
+++ arduinoProcessing/LEDandServoControlGUI/LEDandServoControlGUI.ino
@@ -1,25 +1,28 @@
 /*
- * Attempting to connect read from Processing GUI
+ * Servo and LED control using COM5 and Processing GUI
  * @author Piero Orderique
  * 
- * 
  */
+#include <Servo.h>;
 const int redLED = 10;
 const int yellowLED = 6;
 const int greenLED = 8;
 boolean redState = LOW;
 boolean yellowState = LOW;
 boolean greenState = LOW;
+Servo myServo;
 
 void setup() {
   pinMode(redLED, OUTPUT);
   pinMode(yellowLED, OUTPUT);
   pinMode(greenLED, OUTPUT);
+  myServo.attach(3);
   Serial.begin(9600);
 }
 
-void loop() {
-
+void loop() { 
+  for(int i = 1; i <=180; i++){myServo.write(i);delay(10);}
+  for(int i = 179; i >=0; i--){myServo.write(i);delay(10);}
   if(Serial.available()){
     char val = Serial.read(); //reads the character that came in
     if(val == 'r'){ //if r received 
