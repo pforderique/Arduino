14bb183cbb29b72fbb019e4577c100f22af5181f
Piero F Orderique <45519489+pforderique@users.noreply.github.com>
2021-12-25 13:41:17 -0500

    Delete MyProjects/ArduinoOutput directory

--- MyProjects/ArduinoOutput/core/libb64/cdecode.c.d
+++
@@ -1,7 +0,0 @@
-c:\Users\fabri\OneDrive\Documents\Arduino\MyProjects\ArduinoOutput\core\libb64\cdecode.c.o: \
- C:\Users\fabri\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.4\cores\esp32\libb64\cdecode.c \
- C:\Users\fabri\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.4\cores\esp32\libb64\cdecode.h \
- C:\Users\fabri\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.4/tools/sdk/include/newlib/stdint.h \
- C:\Users\fabri\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.4/tools/sdk/include/newlib/machine/_default_types.h \
- C:\Users\fabri\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.4/tools/sdk/include/newlib/sys/features.h \
- C:\Users\fabri\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.4/tools/sdk/include/newlib/sys/_intsup.h
